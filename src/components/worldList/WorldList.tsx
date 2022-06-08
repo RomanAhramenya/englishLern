d15c610875cad90282234fb73193bb6772f9a39d
Roman <romanahramenya@gmail.com>
2022-06-08 07:45:58 +0300

    commit

---
+++ src/components/worldList/WorldList.tsx
@@ -0,0 +1,72 @@
+import React, { FC, useState } from 'react'
+import { IWorld } from '../../store/slice/WorldAction'
+import Quiz from '../quiz/Quiz'
+import s from './WorldList.module.scss'
+interface IWorldList {
+    dataWorld: IWorld[]
+    currentPage: number
+    action: (idWorld: string) => void
+}
+interface ITrain {
+    world:string
+    translate:string
+}
+const WorldList: FC<IWorldList> = ({ dataWorld, currentPage, action }) => {
+    const [value,setValue] = useState('')
+    const [response,setResponse] = useState(false)
+    const [isTrain, setIsTrain] = useState(false)
+    const [isStartTrain, setIsStartTrain] = useState(false)
+    const [arrayWorldTrain, setArrayWorldTrain] = useState([{world:'',translate:''}])
+    const [question,setQuestion] = useState(1)
+    const addTrainWorld = (world: string, translate: string) => {
+        setArrayWorldTrain([...arrayWorldTrain,{world,translate}])
+      
+    }
+    function getRandomInt(min:number, max:number) {
+        return Math.floor(Math.random() * (max - min)) + min;
+      }
+      
+   console.log(question)
+    const nextHandler = () =>{
+        
+        setQuestion(getRandomInt(1,arrayWorldTrain.length))
+        setResponse(false)
+        setValue('')
+    }
+    const endHandler = () => {
+        setValue('')
+        setResponse(false)
+        setIsTrain(false)
+        setIsStartTrain(false)
+        setArrayWorldTrain([])
+    }
+    const deleteHandler = (el:string) =>{
+        setArrayWorldTrain(arrayWorldTrain.filter(item => item.world !== el))
+    
+    }
+    return (
+        <div className={`${s.worlds} ${isTrain && s.trainStart}`}>
+            {dataWorld.map((el, index) => {
+                if (index + 1 <= currentPage * 10 && index + 1 > (currentPage * 10 - 10))
+                    return <div onDoubleClick={()=> isTrain && deleteHandler(el.world)} onClick={()=> isTrain && addTrainWorld(el.world,el.translate)}  className={`${s.world} ${arrayWorldTrain.find(item => item.world === el.world) && s.active}`} key={index}>
+                        <div>
+                            <span>{index + 1}. {el.world}</span>
+                            <span>----</span>
+                            <span>{el.translate}</span>
+                        </div>
+                        <button onClick={() =>action(el.id)}>Удалить</button>
+                    </div>
+            })}
+            <span onClick={() => setIsTrain(!isTrain)} className={s.train}>Тренировка слов</span>
+            {isTrain && <div className={s.start_train_info}><span>Выберите слова(Двойной клик для отмены слова)</span> <button onClick={()=>setIsStartTrain(true)}>начать</button></div>}
+            {isStartTrain && <div className={s.gameBoard}>
+               <span>{arrayWorldTrain[question].translate}</span> <input placeholder='Ответ' value={value} onChange={e => setValue(e.currentTarget.value)} type='text'/> 
+                <button onClick={()=>setResponse(true)}>ответить</button><button onClick={()=>nextHandler()} >следующий вопрос</button>
+                <button onClick={()=>endHandler()} >Завершить</button>
+                {response && <span>{value === arrayWorldTrain[question].world ? 'верно' : 'не верно'}</span>}
+                </div>}
+        </div>
+    )
+}
+
+export default WorldList
--- src/pages/World/World.tsx
+++ src/pages/World/World.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import AddWorldForm from '../../components/addWorldForm/AddWorldForm'
 import Pagination from '../../components/pagination/Pagination'
+import WorldList from '../../components/worldList/WorldList'
 import { useAppDispatch, useAppSelector } from '../../hooks/redux-hook'
 import { getWorlds, deleteWorld, setWorld } from '../../store/slice/WorldAction'
 import s from './World.module.scss'
@@ -14,11 +15,10 @@ function World() {
             dispatch(getWorlds(auth))
     }, [])
     const handlerAction = (world: string, translate: string, id: string) => {
-        
-        if (auth){
-             dispatch(setWorld({ id: auth, world: { world, translate, id } }))
+        if (auth) dispatch(setWorld({ id: auth, world: { world, translate, id } }))
         }
-           
+    const deleteWorldHandler = (idWorld:string) => {
+        dispatch(deleteWorld({ idUser: String(auth), idWorld }))
     }
     return (
         <div className={s.container}>
@@ -26,22 +26,9 @@ function World() {
            <div className={s.block_status}>
                {world.error  ? <>{world.error}</> : world.isLoading && <>Loading...</>}
             </div> 
-            {world.worlds.map((el, index) => {
-                if (index + 1 <= currentPage * 10 && index + 1 > (currentPage * 10 - 10))
-                    return <div className={`${s.world}`} key={index}>
-                        <div>
-                            <span>{index + 1}. {el.world}</span>
-                            <span>----</span>
-                            <span>{el.translate}</span>
-                        </div>
-                        <button onClick={() => dispatch(deleteWorld({ idUser: String(auth), idWorld: el.id }))}>Удалить</button>
-                    </div>
-            })}
+             <WorldList action={deleteWorldHandler} currentPage={currentPage} dataWorld={world.worlds}/>
             <Pagination pageSwitching={setCurrentPage} totalCount={Number(world.worlds.length)} amountItem={10} />
-            {auth &&
-                <AddWorldForm action={handlerAction}  />
-            }
-
+             <AddWorldForm action={handlerAction}  />
         </div>
     )
 }
